4142d748fbe75bd4e5f0e85f37c964f441ae5960
RAHUL DAGA <rahuldaga@RAHULs-MacBook-Air.local>
2016-11-26 17:16:22 -0500

    fix bind error
    Signed-off-by: RAHUL DAGA <rahuldaga@RAHULs-MacBook-Air.local>

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -302,7 +302,7 @@ int create_server_socket(struct sockaddr_in *server_addr) {
 	}
 
 	/*Bind socket with address struct*/
-	if (bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+	if (bind(server_socket, (struct sockaddr *) server_addr, sizeof(struct sockaddr_in)) < 0) {
 	    std::cerr << "Bind failed" << strerror(errno);
 	    exit(1);
 	}
